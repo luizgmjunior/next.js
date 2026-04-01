4bdfe60369ab3ca689450c7d120cc5eeddd24acc
Will Binns-Smith <wbinnssmith@users.noreply.github.com>
2026-04-01 21:24:26 +0000

    Revert "test: add server HMR test for reactCompiler: true in next.config.js"
    This reverts commit e4e7dfdbf99f8058a842edbea7817dad7d36157f.

--- test/development/app-dir/server-hmr-rc-config/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <p>hello from version 0</p>
-}
--- test/development/app-dir/server-hmr-rc-config/next.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactCompiler: true,
-}
-module.exports = nextConfig
